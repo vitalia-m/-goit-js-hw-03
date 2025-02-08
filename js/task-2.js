5ce9cc2e899fb2a46ae065db51bdc750f240aa48
Vitalia <vitaliya.m05@gmail.com>
2025-02-08 16:33:13 +0100

    all done

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+function makeArray(firstArray, secondArray, maxLenght) {
+  const newArray = firstArray.concat(secondArray);
+  return newArray.length > maxLenght ? newArray.slice(0, maxLenght) : newArray;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
